a167c3ab8023ee7c608f0fe64c50238d1a6291e9
michael <499887880@qq.com>
2019-03-05 13:43:53 +0800

    first commit

---
+++ src/main/java/cn/zy/charg/ChargApplication.java
@@ -0,0 +1,13 @@
+package cn.zy.charg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/zy/charg/ChargApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zy.charg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChargApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
